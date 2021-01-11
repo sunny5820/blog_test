8f869bc96a38f32475f0a83ef3239f85028a3ad4
sunny5820 <35337739+sunny5820@users.noreply.github.com>
2021-01-11 20:44:01 +0900

    mongooseDB connect

--- server/app.js
+++ server/app.js
@@ -1,6 +1,16 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import config from './config';
 
 const app = express();
+const { MONGO_URI } = config;
+
+mongoose.connect(MONGO_URI, {
+    useNewUrlParse: true,
+    useUnifiedTopology: true,
+})
+    .then(() => console.log("MongoDB connecting Success!"))
+    .catch((e) => console.log(e));
 
 app.get('/');
 
