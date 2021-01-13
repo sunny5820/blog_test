dc2e61f98feb41558e5dbef35ae060964b2c23a2
sunny5820 <35337739+sunny5820@users.noreply.github.com>
2021-01-13 14:47:28 +0900

    11강

--- server/app.js
+++ server/app.js
@@ -2,16 +2,40 @@ import express from 'express';
 import mongoose from 'mongoose';
 import config from './config';
 
+import hpp from 'hpp';
+import helmet from 'helmet';
+import cors from 'cors';
+
+
+import morgan from 'morgan';
+
+//Routes
+import postRoutes from './routes/api/post';
+
+
 const app = express();
 const { MONGO_URI } = config;
+
+//배포시 환경
+app.use(hpp());
+app.use(helmet());
+
+app.use(cors({ origin: true, credentials: true }));
+app.use(morgan("dev"));
+
+app.use(express.json());
 
 mongoose.connect(MONGO_URI, {
     useNewUrlParse: true,
     useUnifiedTopology: true,
+    useCreateIndex: true,
 })
     .then(() => console.log("MongoDB connecting Success!"))
     .catch((e) => console.log(e));
 
+//Use routes
 app.get('/');
+app.use('/api/post', postRoutes);
+
 
 export default app;
---
+++ server/routes/api/post.js
@@ -0,0 +1,31 @@
+import express from 'express';
+
+//Model
+import Post from '../../models/post';
+
+const router = express.Router();
+
+//api/post 프론트로 올릴때 프론트랑 백앤드가 주소가 달라야..
+router.get('/', async (req, res) => {
+    const postFindResult = await Post.find();
+    console.log(postFindResult, "All Post Get");
+    res.json(postFindResult);
+});
+
+router.post('/', async (req, res, next) => {
+    try {
+        console.log(req, "req");
+        const { title, contents, fileUrl, creator } = req.body;//구조문의 문법으로 분해/자주쓰는 자바스크립트 문법
+        const newPost = await Post.create({
+            title,
+            contents,
+            fileUrl,
+            creator,
+        });
+        res.json(newPost);
+    } catch (e) {
+        console.log(e);
+    }
+});
+
+export default router;
