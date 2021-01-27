ffcc42a605d06bbe7172a5e78de92d8064164f07
sunny5820 <35337739+sunny5820@users.noreply.github.com>
2021-01-27 11:46:09 +0900

    19강header_Css

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import { Row, Col } from 'reactstrap';
 
 const Header = () => {
     return (
-        <div>Header</div>
+        <div id="page-header" className="mb-3">
+            <Row>
+                <Col md="6" sm="auto" className="text-center m-auto">
+                    <h1>Read Our Blog</h1>
+                    <p>Sunny의 사이드 프로젝트 작업 블로그 입니다.</p>
+                </Col>
+            </Row>
+        </div>
     );
 };
 
