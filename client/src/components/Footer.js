fb1a2311691d4173ae1e34e1b818a7f117d09969
sunny5820 <35337739+sunny5820@users.noreply.github.com>
2021-01-27 11:11:19 +0900

    css-footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import { Row, Col } from 'reactstrap';
 
 const Footer = () => {
-    return <div>Footer</div>
+    const thisYear = () => {
+        const year = new Date().getFullYear();
+        return year;
+    };
+
+    return (
+        <div id="main-footer" className="text-center p-2">
+            <Row>
+                <Col>
+                    <p>Copyright &copy;<spen>{thisYear()}</spen></p>
+                </Col>
+            </Row>
+        </div>
+    )
 };
 
 export default Footer;
