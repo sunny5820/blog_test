607889006af023a84bd46d7602a0b25968a8d063
sunny5820 <35337739+sunny5820@users.noreply.github.com>
2021-01-27 18:42:57 +0900

    22강 reduxSaga

---
+++ client/src/redux/sagas/authSaga.js
@@ -0,0 +1,41 @@
+import axios from 'axios';
+import { call, put, takeEvery, all, fork } from 'redux-saga/effects';
+import { LOGIN_FAILURE, LOGIN_REQUEST } from '../types';
+
+//Login
+
+const loginUserAPI = (loginData) => {
+    console.log(loginData, "loginData");
+    const config = {
+        headers: {
+            "Content-Type": "application/json"
+        }
+    }
+    return axios.post('api/auth', loginData, config)
+};
+
+function* loginUser(action) {
+    try {
+        const result = yield call(loginUserAPI, action.payload)
+        console.log(result);
+        yield put({
+            type: LOGIN_FAILURE,
+            payload: result.data
+        })
+    } catch (e) {
+        yield put({
+            type: LOGIN_FAILURE,
+            payload: e.response
+        })
+    }
+};
+
+function* watchLoginUser() {
+    yield takeEvery(LOGIN_REQUEST, loginUser) //매번 감시
+};
+
+export default function* authSaga() {
+    yield all([
+        fork(watchLoginUser)
+    ])
+};
--- client/src/redux/sagas/index.js
+++ client/src/redux/sagas/index.js
@@ -1,5 +1,12 @@
-import { all } from 'redux-saga/effects';
+import { all, fork } from 'redux-saga/effects';
+import axios from 'axios';
+
+import authSaga from './authSaga';
+import dotenv from 'dotenv';
+dotenv.config()
+
+axios.defaults.baseURL = process.env.REACT_APP_BASIC_SERVER_URL
 
 export default function* rootSaga() { //function* 제너레이터함수로 여러개 함수를 반환한다.
-    yield all([]);
+    yield all([fork([authSaga])]);
 }
